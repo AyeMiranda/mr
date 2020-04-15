9b7bc1c958d78c4d232760e99fda8bdc6ab02678
arielle-rserver <arielle-rserver>
2020-04-15 14:55:41 +0000

    create new functions

--- R/hello.R
+++ R/hello.R
@@ -1,7 +1,19 @@
-#' Hello
+#' Binance API to get most recent USD price of BTC
 #' @export
-hello <- function() {
-  print("Hello, world")
+#' @param retried the number of retries previously done
+#' @importFrom binancer binance_coins_prices
+get_bitcoin_price <- function(retried = 0) {
+  tryCatch(binance_coins_prices()[symbol == 'BTC', usd],
+           error = function(e) {
+             # exponential backoff retries
+             Sys.sleep(1)
+             get_bitcoin_price(retried = retried + 1)})
 }
 
-
+#' Formatter function for HUF
+#' @param x number
+#' @export
+#' @importFrom scales dollar
+forint <- function(x) {
+  dollar(x, prefix = '', suffix = 'Ft')
+}
